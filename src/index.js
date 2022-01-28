b09452e056136420aa8e6a02a8e90abb31c429d4
yuliaelkina <todg@mail.ru>
2022-01-28 12:33:51 +0300

    api class done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-import './js/app';
 import './css/style.css';
+import './js/app';
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
-import TicketManager from './ticketManager';
+import API from './api';
 
-const widget = new TicketManager('.helpdesk__container');
-widget.init();
+let user;
+const api = new API('https://ajs7server.herokuapp.com/');
+
+const welcomeForm = document.querySelector('.welcome__form');
+const welcomeInput = document.querySelector('.welcome__input');
+welcomeForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(welcomeInput.value);
+})
